39de7f99ba854ae83ada15dc1f4c15efa8ed65a3
vecalciskay <vladimmri.calderon@gmail.com>
2022-10-04 08:54:20 -0400

    Clase Martes 4

--- src/main/java/nur/p3/arboles/Arbol.java
+++ src/main/java/nur/p3/arboles/Arbol.java
@@ -9,13 +9,106 @@ public class Arbol<E> {
         raiz = null;
     }
 
+    public void anadir(E o, String id, String idpadre) {
+        Nodo<E> nuevo = new Nodo<>(id, o);
+
+        if (idpadre == null) {
+            raiz = nuevo;
+            return;
+        }
+
+        Nodo<E> padre = raiz.buscar(idpadre);
+        if (padre == null) {
+            // excepcion horrible
+            return;
+        }
+
+        padre.anadirHijo(nuevo);
+    }
+
+    @Override
+    public String toString() {
+        if (raiz == null)
+            return "[VACIO]";
+        return raiz.toString();
+    }
+
     class Nodo<E> {
         private E contenido;
+        private String identificador;
         private Lista<Nodo<E>> hijos;
+        private Nodo<E> padre;
 
-        public Nodo(E o) {
+        public Nodo(String id, E o) {
+            identificador = id;
             contenido = o;
             hijos = new Lista<>();
+            padre = null;
+        }
+
+        public E getContenido() {
+            return contenido;
+        }
+
+        public void setContenido(E contenido) {
+            this.contenido = contenido;
+        }
+
+        public String getIdentificador() {
+            return identificador;
+        }
+
+        public Lista<Nodo<E>> getHijos() {
+            return hijos;
+        }
+
+        public Nodo<E> getPadre() {
+            return padre;
+        }
+
+        public void setPadre(Nodo<E> padre) {
+            this.padre = padre;
+        }
+
+        public void anadirHijo(Nodo<E> nuevo) {
+            nuevo.setPadre(this);
+            this.hijos.adicionar(nuevo);
+        }
+
+        public Nodo<E> buscar(String idpadre) {
+            if (this.identificador.equals(idpadre)) {
+                return this;
+            }
+
+            for (Nodo<E> hijo:
+                 hijos) {
+                Nodo<E> encontrado = hijo.buscar(idpadre);
+                if (encontrado != null)
+                    return encontrado;
+            }
+
+            return null;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder resultado = new StringBuilder();
+            resultado.append(identificador);
+
+            if (hijos.getTamano() == 0)
+                return resultado.toString();
+
+            //  A->(B->(E,F),C)
+            resultado.append("->(");
+            String conector = "";
+            for (Nodo<E> hijo :
+                    hijos) {
+                resultado.append(conector).append(hijo.toString());
+                conector = ",";
+            }
+            resultado.append(")");
+
+            return resultado.toString();
         }
     }
 }
--- src/test/java/nur/p3/qa/ArbolTest.java
+++ src/test/java/nur/p3/qa/ArbolTest.java
@@ -1,7 +1,11 @@
 package nur.p3.qa;
 
+import nur.p3.arboles.Arbol;
+import nur.p3.shared.Persona;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Date;
 
 public class ArbolTest {
 
@@ -13,11 +17,18 @@ public class ArbolTest {
                 Nodo C debajo de A
                 Nodo E debajo de B
                 Nodo F debajo de B
-        */
+*/
+        Arbol<String> a = new Arbol<>();
+        a.anadir("A", "A", null);
+        a.anadir("B", "B", "A");
+        a.anadir("C", "C", "A");
+        a.anadir("E", "E", "B");
+        a.anadir("F", "F", "B");
 
         // Act
         String s =  a.toString();
 
-        Assertions.assertEquals("A->(B->(E , F) , C)", s);
+        // Assert
+        Assertions.assertEquals("A->(B->(E,F),C)", s);
     }
 }
